3eeda25203ae6d7ab13a438ce9d07955741b0b4f
Bennett Kanuka <bennett@climateengine.com>
2021-07-23 17:20:15 -0400

    add cloudbuild.yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -x
 
 PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 PROJECT_NAME=$(gcloud projects describe ${PROJECT_ID} --format="value(name)")
-gcloud config set project $PROJECT_ID
+gcloud config set project "$PROJECT_ID"
 
 rm -rf .terraform*
 
