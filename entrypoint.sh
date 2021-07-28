78fc471d465ec453b7e880aaf5d85f5b24ba3238
Bennett Kanuka <bennett@climateengine.com>
2021-07-28 06:51:48 -0400

    param more things

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ echo project_id = \"${PROJECT_ID}\" > terraform.tfvars
 echo project_name = \"${PROJECT_NAME}\" >> terraform.tfvars
 echo app_name = \"${APP_NAME}\" >> terraform.tfvars
 echo namespace = \"${NAMESPACE}\" >> terraform.tfvars
+echo cloud_sql_pass = \"${SQL_PASSWORD}\" >> terraform.tfvars
 
 terraform init
 terraform apply -auto-approve
