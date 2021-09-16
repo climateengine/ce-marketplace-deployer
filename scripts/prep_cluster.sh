aae116622a9a7e7eafc6e47e68f678af90cea307
Bennett Kanuka <bennett@climateengine.com>
2021-09-15 20:17:06 -0400

    fix cloudsql proxy

--- scripts/prep_cluster.sh
+++ scripts/prep_cluster.sh
@@ -9,7 +9,7 @@ DEPLOYER_SA=climate-engine-deployer
 # export PROJECT_ID=[gcp_project_id]
 gcloud config set project $PROJECT_ID
 gcloud services enable container.googleapis.com
-gcloud beta container clusters create "climate-engine-1" --zone "us-central1-c" --machine-type "e2-medium" --num-nodes "3" --node-locations "us-central1-c"
+gcloud beta container clusters create "climate-engine-1" --zone "us-central1-c" --machine-type "e2-medium" --num-nodes "6" --node-locations "us-central1-c"
 gcloud container clusters get-credentials climate-engine-1 --zone "us-central1-c"
 gcloud iam service-accounts create $DEPLOYER_SA --display-name="Climate Engine Service Account"
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$DEPLOYER_SA@${PROJECT_ID}.iam.gserviceaccount.com" --role="roles/owner"
